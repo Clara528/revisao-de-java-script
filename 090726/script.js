1773559ed4ffa8805b3b116134641da3c613dc15
Ana Clara <ana30380286@gmail.com>
2026-07-09 15:26:03 -0300

    Add files via upload

---
+++ 090726/script.js
@@ -0,0 +1,19 @@
+
+let opcao
+
+opcao=prompt("digite um valor:")
+opcao=Number(opcao)
+
+if(opcao>=0){
+    console.log("positivo")
+}else if(opcao<0){
+    console.log("negativo")
+}else{
+    console.log("Erro")
+}
+opcao= opcao + 1
+console.log(opcao)
+
+switch(opcao){
+    
+}
